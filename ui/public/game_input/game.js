18727c78332c904e1cd015ee20f0543d2ab8bd41
Evan White <evanwht1@gmail.com>
2020-06-21 11:05:20 -0500

    copy over old ui

---
+++ ui/public/game_input/game.js
@@ -0,0 +1,25 @@
+function checkUnique(p, o) {
+  if ($(p).val() == $(o).val()) {
+    $(p).css('color', 'red');
+  } else {
+    $(p).css('color', 'black');
+  }
+}
+
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
