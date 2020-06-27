24008859b709221f84bc5cfc75e5dd19900f8f82
Evan White <evanwht1@gmail.com>
2020-06-26 23:03:55 -0500

    react nav bar

--- ui/react-ui/src/index.js
+++ ui/react-ui/src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import { Navigation } from './Nav';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Navigation />
+    <Switch>
+      <Route path="/">
+        <App />
+      </Route>
+      <Route path="/games">
+        <App />
+      </Route>
+      <Route path="/new/game">
+        <App />
+      </Route>
+      <Route path="/new/player">
+        <App />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
