8202e9c53d0504e68f037f3f1e3c8b498f1bc79b
Evan White <evanwht1@gmail.com>
2020-06-29 22:17:12 -0500

    player form in react

--- ui/react-ui/src/PlayerForm.js
+++ ui/react-ui/src/PlayerForm.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react'
-
+import Form from 'react-bootstrap/Form';
+import FormLabel from 'react-bootstrap/FormLabel';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 export class PlayerForm extends Component {
     render() {
         return (
-            <div class="container">
-                <div class="row">
-                    <div class="col-8 mx-auto undernav text-center">
-                        <div class="pb-4 pt-4">
-                            <h3>New Player</h3>
-                        </div>
-                        <form class="col-md-8 col-sm-10 mx-auto boxed pt-3 needs-validation" method="POST" action="api/players" novalidate>
-                            <div class="form-group col">
-                                <label class="col col-form-label-lg text-left text-white">First Name</label>
-                                <div class="">
-                                    <input name="firstName" type="text" class="centering form-control form-fill" required />
-                                </div>
-                            </div>
-                            <div class="form-group col">
-                                <label class="col col-form-label-lg text-left text-white">Last Name</label>
-                                <div class="">
-                                    <input name="lastName" type="text" class="centering form-control form-fill" required />
-                                </div>
-                            </div>
-                            <div class="form-group col">
-                                <label class="col col-form-label-lg text-left text-white">Knick Name</label>
-                                <div class="">
-                                    <input name="nickName" type="text" class="centering form-control form-fill" required />
-                                </div>
-                            </div>
-                            <div class="form-group col">
-                                <label class="col col-form-label-lg text-left text-white">Email</label>
-                                <div class="">
-                                    <input name="email" type="email" class="centering form-control form-fill" required />
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary btn-lg mb-4 mt-4">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col sm="8" className="mx-auto undernav">
+                        <h3 className="pb-4 pt-4 text-white text-center">New Player</h3>
+                        <Form className="col-md-10 col-sm-12 mx-auto boxed pt-3" method="POST" action="api/players">
+                            <Form.Group controlId="FirstName">
+                                <FormLabel className="text-white">First Name</FormLabel>
+                                <Form.Control name="firstname" type="text" className="mb-3 mt-2" />
+                            </Form.Group>
+                            <Form.Group controlId="LastName">
+                                <FormLabel className="text-white">Last Name</FormLabel>
+                                <Form.Control name="lastname" type="text" className="mb-3 mt-2" />
+                            </Form.Group>
+                            <Form.Group controlId="KnickName">
+                                <FormLabel className="text-white">Knick Name</FormLabel>
+                                <Form.Control name="kickname" type="text" className="mb-3 mt-2" />
+                            </Form.Group>
+                            <Form.Group controlId="Email">
+                                <FormLabel className="text-white">Email</FormLabel>
+                                <Form.Control name="email" type="email" className="mb-3 mt-2" />
+                            </Form.Group>
+                            <Form.Row>
+                                <Col sm="6" className="mx-auto">
+                                    <Button variant="primary" size="lg" type="submit" block className="mb-4 mt-2">Submit</Button>
+                                </Col>
+                            </Form.Row>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
